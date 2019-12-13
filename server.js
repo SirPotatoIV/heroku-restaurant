2504df178a09a50fc7998a8c00ee16b0d9009b1d
SirPotatoIV <otoolej33@gmail.com>
2019-12-13 14:01:20 -0600

    Possibly corrected?

--- server.js
+++ server.js
@@ -75,7 +75,7 @@
         res.sendFile(path.join(__dirname, "home.html"));
     });
 
-    app.get("/reservations", function(req, res) {
+    app.get("/reserve", function(req, res) {
         res.sendFile(path.join(__dirname, "reserve.html"));
     });
     // Starts the server to begin listening
