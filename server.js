73e58843b0cb5e35fae53505a1d18bab328b490c
SirPotatoIV <otoolej33@gmail.com>
2019-12-13 14:15:51 -0600

    Added code to get reservation info when button is clicked. Added id to button for event listener.

--- server.js
+++ server.js
@@ -84,3 +84,19 @@
         console.log("App listening on PORT " + PORT);
     });
     // * Use jQuery to run AJAX calls to GET and POST data from users to the Express server
+
+      // Question: What does this code do?
+      document.getElementById("submit-reservation").addEventListener("click", function(event) {
+        event.preventDefault();
+        var newReservations = {
+          name: document.getElementById("reserve_name").value.trim(),
+          role: document.getElementById("reserve_phone").value.trim(),
+          age: document.getElementById("reserve_email").value.trim(),
+        };
+        // Question: What does this code do??
+        axios.post("/api/reservations", newReservations)
+          .then(function(data) {
+            console.log("add.reservation", data);
+            alert("Added Reservation...");
+          });
+      });
